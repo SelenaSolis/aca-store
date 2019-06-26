b36ad9433fa93254cec5facc046e281262f8de7b
Selena Solis <lenansolis@gmail.com>
2019-06-26 15:08:41 -0500

    search function

--- public/index.js
+++ public/index.js
@@ -1,1 +1,23 @@
-//stuff
+var myProducts = products;
+let productsDiv = document.getElementById("products");
+let newProdName = "";
+
+
+myProducts.map(product =>{
+    newProdName += `<div><div><img src=${product.imgUrl}></div><div><h3>${product.name}</h3></div><div>${product.description}</div></div>`;
+})
+productsDiv.innerHTML = newProdName;
+
+
+function searchFunc(){
+    newProdName = ""
+    let searchText = document.getElementById("searchText").value;
+    myProducts.map(product =>{
+        let name = product.name.toLowerCase();
+        let desc = product.description;
+        if(name.includes(searchText) || desc.includes(searchText)){
+            newProdName += `<div><div><img src=${product.imgUrl}></div><div><h3>${product.name}</h3></div><div>${product.description}</div></div>`;
+        }
+    })
+    productsDiv.innerHTML = newProdName;
+}
