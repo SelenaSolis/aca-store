c8c7677377599daddeb610e2f0545c75bd48ad59
Selena Solis <lenansolis@gmail.com>
2019-06-30 18:13:47 -0500

    more info and session storage

--- public/index.js
+++ public/index.js
@@ -1,10 +1,31 @@
+
+let cartItems = [];
+let sessionCartItems = sessionStorage.getItem("cart");
+if(sessionCartItems){
+    cartItems = JSON.parse(sessionCartItems);
+}
+
 function listProducts(products){
+    let cartDiv = document.getElementById("cart");
     let productsDiv = document.getElementById("products");
-    let newProdName = "";
+    let newProd = '';
     products.map(product =>{
-        newProdName += `<div><div><img src=${product.imgUrl}></div><div><h3>${product.name}</h3></div><div>${product.description}</div></div>`;
+        newProd += `<div id = 'product'>
+                <a onclick = 'moreInfo(${product.id})'><div><img src=${product.imgUrl}></div>
+                <div><h4 id ='${product.id}'>${product.name}</h4></a></div>
+                <div><h5>${product.description}</h5></div>
+                <div>${product.price}</div>
+                <div>rating: ${product.rating}</div>
+                <div><button onclick = 'moreInfo(${product.id})' id = 'moreInfo'>More Information</button></div>
+                <div><button onclick = 'addToCart(${product.id})'>ADD TO CART</button></div>
+                <div id = 'moreInfo${product.id}'></div>
+            </div>`;
     })
-    productsDiv.innerHTML = newProdName;
+
+    if (productsDiv){
+        productsDiv.innerHTML = newProd; 
+    }
+    cartDiv.innerHTML = JSON.parse(sessionStorage.getItem("cart")).length;
 }
 listProducts(products);
 
@@ -15,3 +36,36 @@ function searchFunc(){
         prod.name.toLowerCase().includes(searchText) || prod.description.toLowerCase().includes(searchText));
     listProducts(filteredProducts)
 }
+
+
+function addToCart(prodId){
+    products.map(product => {
+        if(product.id === prodId){
+            cartItems.push(product);
+        }
+    })
+    console.log(cartItems);
+    sessionStorage.setItem("cart", JSON.stringify(cartItems));
+    listProducts(products);
+}
+
+
+function moreInfo(prodId){
+    let moreInfoDiv = '';
+    let viewProd = products.filter(prod =>
+        prod.id === prodId
+    )
+    listProducts(viewProd);
+    let productDiv = document.getElementById(`moreInfo${prodId}`);
+    viewProd[0].reviews.map(review =>
+        moreInfoDiv += `<div class = 'review'><div>${review.description}</div>
+            <div>${review.rating}</div>
+            </div>`
+        )
+    productDiv.innerHTML = moreInfoDiv;
+
+
+    
+}
+
+
